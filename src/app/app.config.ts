7321c6793c84226244cf66b640bf227ba7e99e8d
flexGURU <mukunajohn329@gmail.com>
2025-11-27 18:37:13 +0300

    feat: implement device detail feature with data fetching, loading states, and error handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Material from '@primeuix/themes/material';
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
+    provideTanStackQuery(new QueryClient()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideAnimationsAsync(),
